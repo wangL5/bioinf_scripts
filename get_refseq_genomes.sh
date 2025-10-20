4ca8429e353437a9acc55c2dd904c7f1e8f54c0c
Lu Wang <65783528+wangL5@users.noreply.github.com>
2025-10-20 15:04:01 -0700

    added get_refseq_genomes.sh
    This script downloads NCBI refseq representative genomes from a specified list of species.

---
+++ get_refseq_genomes.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# this script downloads NCBI refseq representative genomes from a list of species. Genus level ok.
+
+# assign argument file to variable
+input_file="$1"
+
+if [ -z "$input_file" ]; then
+        echo "Usage: $0 <input_file>"
+        exit 1
+fi
+
+while IFS= read -r species; do
+        echo "Downloading $species"
+        datasets download genome taxon "$species" --reference \
+                --filename "${species// /_}.genomes.zip"
+done < "$input_file"
